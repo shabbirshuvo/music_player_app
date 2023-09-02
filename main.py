38934121a45b95c1f27e4e0957fadeffededbaf4
shabbirshuvo <shabbir.unirostock@gmail.com>
2023-09-02 23:35:53 +0200

    created directory selection & UI enhancement

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ import time
 import kivy
 from kivy.app import App
 from kivy.clock import Clock
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.filechooser import FileChooserListView
 from kivy.uix.image import Image
 from kivy.uix.label import Label
+from kivy.uix.popup import Popup
 from kivy.uix.slider import Slider
 from kivymd.uix.relativelayout import MDRelativeLayout
 from kivymd.uix.button import MDIconButton
@@ -44,6 +48,13 @@ def resize_image(input_image_path, output_image_path, desired_size):
 class MusicPlayer(MDApp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.location_label = None
+        self.filechooser = None
+        self.progress_bar = None
+        self.music_dir = None
+        self.select_button = None
+        self.file_chooser = None
+        self.now_playing_label = None
         self.previous_button = None
         self.volume_slider = None
         self.next_button = None
@@ -64,6 +75,55 @@ class MusicPlayer(MDApp):
     # def update_progress_bar(self, value):
     #     if self.progress_bar.value < 100:
     #         self.progress_bar.value += 1
+
+    def update_location_label(self, dt):
+        self.location_label.text = "    Current Location: " + self.music_dir
+
+    def move_song_label(self, dt):
+        text = self.location_label.text
+        updated_text = text[1:] + text[0]
+        self.location_label.text = updated_text
+
+    def open_folder_dialog(self, instance):
+        layout = BoxLayout(orientation='vertical', size_hint=(1, 1))
+
+        # Note that we're using dirselect=True here.
+        filechooser = FileChooserListView(path=os.getcwd(), dirselect=True)
+
+        select_button = Button(text="Select", size_hint_y=None, height=30, on_release=self.select_folder)
+        cancel_button = Button(text="Cancel", size_hint_y=None, height=30, on_release=self.dismiss_popup)
+
+        layout.add_widget(filechooser)
+        layout.add_widget(select_button)
+        layout.add_widget(cancel_button)
+
+        # Here we're creating a reference to the filechooser to be able to fetch the selection later.
+        self.filechooser = filechooser
+
+        self._popup = Popup(title="Choose Folder", content=layout, size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    # def selected_directory(self, instance):
+    #     selected_dir = self.file_chooser.path
+    #     # Use the directory path for your purpose here
+    #     print(f"Selected directory: {selected_dir}")
+    #     self.music_dir = selected_dir
+
+    def select_folder(self, instance):
+        # Get the selected folder. If multiple directories can be selected,
+        # this would return a list. Otherwise, it returns the current directory.
+        folder = self.filechooser.path
+        print("Selected folder:", folder)
+        self.dismiss_popup(instance)
+        self.music_dir = folder
+        self.music_files = find_files(self.music_dir, ".mp3")
+        if self.sound:
+            self.stop_music(instance)
+            self.play_music(instance)
+        Clock.schedule_once(self.update_location_label, 0.1)
+
+    def dismiss_popup(self, instance):
+        self._popup.dismiss()
 
     def play_next_song(self, instance):
         self.current_song_index += 1
@@ -116,6 +176,9 @@ class MusicPlayer(MDApp):
         self.current_time_label.text = current_time
         self.total_time_label.text = total_time
 
+        if current_time == total_time:
+            self.play_next_song(self.next_button)
+
     # def play_music(self, instance):
     #     self.elapsed_time = 0
     #     self.play_button.disabled = True
@@ -143,6 +206,7 @@ class MusicPlayer(MDApp):
         Clock.unschedule(self.update_current_time_label)
 
         self.elapsed_time = 0
+        self.now_playing_label.opacity = 1
         self.play_button.disabled = True
         self.stop_button.disabled = False
         self.next_button.disabled = False
@@ -162,6 +226,7 @@ class MusicPlayer(MDApp):
 
     def stop_music(self, instance):
         self.sound.stop()
+        self.now_playing_label.opacity = 0
         self.play_button.disabled = False
         self.stop_button.disabled = True
         self.next_button.disabled = True
@@ -172,6 +237,7 @@ class MusicPlayer(MDApp):
         self.current_time_label.text = "00:00"
         self.total_time_label.text = "00:00"
         self.progress_bar.value = 0
+        self.volume_slider.value = 0.5
 
     def volume_changed(self, instance, value):
         self.sound.volume = self.volume_slider.value
@@ -181,7 +247,13 @@ class MusicPlayer(MDApp):
         self.title = "Music Player"
 
         # getting the songs from the music folder
-        self.music_files = find_files(".", ".mp3")
+        if self.music_dir is None:
+            self.music_dir = os.getcwd()
+        self.music_files = find_files(self.music_dir, ".mp3")
+
+        self.now_playing_label = Label(text="Now Playing", pos_hint={"center_x": 0.5, "center_y": 0.9},
+                                       font_size=50, opacity=0)
+        self.root.add_widget(self.now_playing_label)
 
         self.song_label = Label(text="", pos_hint={"center_x": 0.5, "center_y": 0.85}, font_size=40)
         self.root.add_widget(self.song_label)
@@ -228,6 +300,17 @@ class MusicPlayer(MDApp):
                                     size_hint=(0.35, 0.1))
         self.volume_slider.bind(value=self.volume_changed)
         self.root.add_widget(self.volume_slider)
+        self.file_chooser = FileChooserListView(path='.', dirselect=True)
+
+        self.select_button = Button(text="Select Directory", on_release=self.open_folder_dialog, size_hint=(.5, 0.05),
+                                    pos_hint={"center_x": 0.5, "center_y": 0.09})
+        self.root.add_widget(self.select_button)
+        self.location_label = Label(text="    Current Location: " + self.music_dir,
+                                    pos_hint={"center_x": 0.5, "center_y": 0.05})
+        self.root.add_widget(self.location_label)
+
+        Clock.schedule_interval(self.move_song_label, 0.2)
+
         return self.root
 
 
